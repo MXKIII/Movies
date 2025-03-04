69e3ac4c549ba840f2e21ada562284a9b74bcaa2
Pret Secretariat <pretsecretariat@administration.paris-sorbonne.fr>
2025-03-04 12:00:54 +0100

    return table with id

--- index.js
+++ index.js
@@ -12,6 +12,16 @@ app.get('/movies', (request,response)=>{
     return response.json(movies)
 })
 
+app.get('/movies/:id', (request,response)=>{
+    const movieID = request.params.id
+    const movieByID= movies.find(movie => movie.id == parseInt(movieID))
+    if(!movieByID){
+        return response.status(404).json ({message: 'Movie not found'})
+    }
+    return response.json(movieByID)
+})
+
+
 app.listen(3000,()=>{
     console.log('server is running on port 3000')
 })
